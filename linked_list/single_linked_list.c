55652ccd9f355dc51394da9fe74984e13d16803f
Michalina Oleksy <layika@protonmail.com>
2016-06-20 22:43:16 +0200

    Changed list_insert and list_free

--- linked_list/single_linked_list.c
+++ linked_list/single_linked_list.c
@@ -24,13 +24,15 @@ node* list_create(data_t data){
 }
 
 
-void list_free(node* first){
-  while (first != NULL)
+void list_free(node** head){
+  node* current = *head;
+  while (current != NULL)
     {
-      node* tmp = first;
-      first = first->next;
+      node* tmp = current;
+      current = current->next;
       free(tmp);
     }
+  *head = NULL; 
 }
 
 void list_append(node** list, int new_element){
@@ -46,183 +48,220 @@ void list_append(node** list, int new_element){
     node* current = *list;
     while (current->next)
       current = current->next;
-    current->next = new_node;
+      current->next = new_node;
+    }
   }
-}
 
 
-int list_pop(node* list){
-
-  if (!list)
-    return -1;
+  int list_pop(node** list){
+    int result;
   
-  node* prev_node = NULL;
-  while (list->next != NULL) {
-    prev_node = list;
-    list = list->next; 
-  } 
-  
-  if (prev_node)
-    prev_node->next = NULL;
+    if (!(*list))
+      return -1;
 
-  int result = list->data; 
-  free(list);
-  return result;
-}
-
-void list_insert(node** list, int new_element, int position){
-  node* left;
-  node* new_node = (node*)malloc(sizeof(node));
-  CHECK_ALLOC(new_node);
-  node *right = *list;
-
-  if (*list == NULL) {
-    new_node->data = new_element;
-    new_node->next = NULL;
-    *list = new_node; 
-  }
-  
-  else {
-    if (position == 0){
-      new_node->data = new_element;
-	new_node->next = *list;
-	*list = new_node; 
-      }
-  
-      else {
-	for (int i = 0; i < position; ++i){
-	  left = right;
-	  right = right->next;
-	} 
-	new_node->data = new_element;
-	left->next = new_node;
-	new_node->next = right;
-      }
-  }
-}
-
-int list_remove(node** list, int position){
-  node* left;
-  node* right = *list;
-  int removed_el;
-
-  if (!*list)
-    return -1;
-
-  if (!(*list)->next) {
-    removed_el = (*list)->data;
-    free(*list);
-    *list = NULL;
-    return removed_el;
-  }
-    
-  else {
-    if (position == 0){
-      removed_el = (*list)->data; 
-      *list = (*list)->next; 
-      free(right);
+    if (!(*list)->next) {
+      result = (*list)->data;
+      free(*list);
+      *list = NULL;
+      return result;
     }
     else {
-      for (int i = 0; i < position; ++i){
-	left = right;
-	right = right->next;
-      }
-      left->next = right->next;
-      removed_el = right->data;
-      free(right);
+      node* prev_node = NULL;
+      while ((*list)->next != NULL) {
+	prev_node = *list;
+	*list = (*list)->next; 
+      } 
+  
+      if (prev_node)
+	prev_node->next = NULL;
+
+      result = (*list)->data; 
+      free(*list);
+      return result;
     }
-    return removed_el;
   }
-}
-  
-void test_list_create(void) {
-  node* list = list_create(1);
-  
-  ASSERT(list->data == 1);
-  
-  list_free(list);
-}
 
+  void list_insert(node** list, int new_element, int position){
+    node* new_node = (node*)malloc(sizeof(node));
+    CHECK_ALLOC(new_node);
+  
+    new_node->data = new_element;
 
-void test_list_append(void) {
-  node* list = list_create(1);
+    if (*list == NULL) {
+      new_node->next = NULL;
+      *list = new_node; 
+    }
   
-  list_append(&list, 2);
+    else if (position == 0) {
+      new_node->next = *list;
+      *list = new_node; 
+    }
   
-  ASSERT(list->data == 1);
-  ASSERT(list->next->data == 2);
-  
-  list_free(list);
-}
+    else {
+      node* prev = NULL;
+      node* current = *list;
+      int counter = 0;
 
-void test_list_append_many(void) {
-  node* list = list_create(0); 
-  ASSERT(list->data == 0);
-  
-  node* iterator = list;
-  for(int i=1; i<15; ++i) {
-    list_append(&list, i);
-	iterator = iterator->next;
-	ASSERT(iterator->data == i);
+      while (current->next) {
+	prev = current;
+	current = current->next; 
+	++counter;
+      
+	if (counter == position){
+	  prev->next = new_node;
+	  new_node->next = current;
+	  return;
+	}
       }
-      list_free(list);
-}
-
-void test_append_to_empty() {
-  node* head = NULL;
-  list_append(&head, 1);
-  ASSERT(head != NULL);
-  ASSERT(head->data == 1);
-  ASSERT(head->next == NULL);
-  list_free(head);
-}
-
-void test_list_pop(void) {
-  node* list = list_create(1); 
-  ASSERT(list_pop(list) == 1); 
-  list = NULL;
-}
-
-void test_list_insert(void){
-  node* list = list_create(0); 
-  node* iterator = list;
-  for(int i=1; i<5; ++i) {
-    list_append(&list, i);
-    iterator = iterator->next; 
+      current->next = new_node;
+    }
   }
-  list_insert(&list, 16, 2);
-  ASSERT(list->next->next->data == 16);
-  list_free(list);
-}
 
-void test_list_insert_to_empty(void){
-  node* head = NULL;
-  list_insert(&head, 2, 0);
-  ASSERT(head != NULL);
-  ASSERT(head->data == 2);
-  ASSERT(head->next == NULL);
-  list_free(head);
-}
+  int list_remove(node** list, int position){
+    int removed_el;
+    if (!*list)
+      return -1;
 
-/* void test_list_remove(void){ //todo
-   node* list = list_create(100); 
-   node* iterator = list;
-   for(int i=1; i<7; ++i) {
-   list_append_element_end(list, i);
-   iterator = iterator->next; 
-   } 
-   }
-*/
+    if (!(*list)->next) {
+      removed_el = (*list)->data;
+      free(*list);
+      *list = NULL;
+      return removed_el;
+    }
+    
+    else {
+      if (position == 0){
+	node* temp = *list;
+	removed_el = (*list)->data; 
+	*list = (*list)->next; 
+	free(temp);
+      }
+      else {
+	int counter = 0;
+	node* temp = *list;
+	while (temp->next) {
+	  temp = temp->next;
+	  ++counter; 
+	  if (counter == (position-1)) 
+	    break; 
+	} 
+	if (temp->next->next == NULL){
+	  removed_el = temp->next->data;
+	  node* remove = temp->next;
+	  temp->next = NULL;
+	  free(remove);
+	}
+	else {
+	  removed_el = temp->next->data;
+	  node* remove = temp->next;
+	  temp->next = temp->next->next;
+	  free(remove);
+	} 
+      }
+      return removed_el;
+    }
+  }
+  void test_list_create(void) {
+    node* list = list_create(1);
+  
+    ASSERT(list->data == 1);
+  
+    list_free(&list);
+  }
 
-int main(void){
-  test_list_create();
-  test_list_append();
-  test_list_append_many(); 
-  test_append_to_empty();
-  test_list_pop();
-  test_list_insert();
-  test_list_insert_to_empty();
-  //test_list_remove();
 
-  return 0;
-}
+  void test_list_append(void) {
+    node* list = list_create(1);
+  
+    list_append(&list, 2);
+  
+    ASSERT(list->data == 1);
+    ASSERT(list->next->data == 2);
+  
+    list_free(&list);
+  }
+
+  void test_list_append_many(void) {
+    node* list = list_create(0); 
+    ASSERT(list->data == 0);
+  
+    node* iterator = list;
+    for(int i=1; i<15; ++i) {
+      list_append(&list, i);
+      iterator = iterator->next;
+      ASSERT(iterator->data == i);
+    }
+    list_free(&list);
+  }
+
+  void test_list_append_to_empty() {
+    node* head = NULL;
+    list_append(&head, 1);
+    ASSERT(head != NULL);
+    ASSERT(head->data == 1);
+    ASSERT(head->next == NULL);
+    list_free(&head);
+  }
+
+  void test_list_pop(void) {
+    node* list = list_create(1); 
+    ASSERT(list_pop(&list) == 1); 
+    list = NULL;
+  }
+
+  void test_list_insert(void){
+    node* list = list_create(0); 
+    node* iterator = list;
+    for(int i=1; i<5; ++i) {
+      list_append(&list, i);
+      iterator = iterator->next; 
+    }
+    list_insert(&list, 16, 2);
+    ASSERT(list->next->next->data == 16);
+    list_free(&list);
+  }
+
+  void test_list_insert_to_empty(void){
+    node* head = NULL;
+    list_insert(&head, 2, 0);
+    ASSERT(head != NULL);
+    ASSERT(head->data == 2);
+    ASSERT(head->next == NULL);
+    list_free(&head);
+  }
+
+  void test_list_insert_to_right_position(void){
+    node* head = list_create(1);
+    list_append(&head, 2); 
+    list_insert(&head, 3, 1);
+    ASSERT(head->data == 1);
+    ASSERT(head->next->data == 3);
+    ASSERT(head->next->next->data == 2);
+    ASSERT(head->next->next->next == NULL);
+    list_free(&head);
+  }
+
+  void test_list_remove(void){ 
+    node* head = list_create(100); 
+    list_append(&head, 1);
+    list_append(&head, 2);
+    list_append(&head, 3); 
+    ASSERT(list_remove(&head, 2) == 2);
+    ASSERT(head->next->next->data == 3);
+    list_free(&head);
+  }
+
+
+  int main(void){
+    test_list_create();
+    test_list_append();
+    test_list_append_many(); 
+    test_list_append_to_empty();
+    test_list_pop();
+    test_list_insert();
+    test_list_insert_to_empty();
+    test_list_insert_to_right_position();
+    test_list_remove();
+
+    return 0;
+  }
