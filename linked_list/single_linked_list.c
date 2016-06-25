c228aa11cd41bb8c35f34c6f37ccaa6f698a7119
Michalina Oleksy <layika@protonmail.com>
2016-06-25 15:26:56 +0200

    Attempt to fix list_remove

--- linked_list/single_linked_list.c
+++ linked_list/single_linked_list.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define ASSERT(x) printf("Assertion: " #x " = %d\n", (x))
+#define ASSERT(x) printf("Assertion: " #x " = %d\n", (x))		/* \
+									 if (!x) \
+									 printf("Error in function %s, line %d", __func__, __LINE__);	\*/
 
 typedef int data_t;
 
 typedef struct node {
-  data_t data;
-  struct node *next;
+data_t data;
+struct node *next;
 } node;
 
 #define CHECK_ALLOC(ptr)						\
@@ -52,58 +54,59 @@ void list_append(node** list, int new_element){
     }
 }
 
-
+//If the list is empty list_pop returns -1
 int list_pop(node** list){
   int result;
+  node* tmp = *list;
   
-  if (!(*list))
+  if (!tmp)
     return -1;
 
-  if (!(*list)->next) {
-    result = (*list)->data;
-    free(*list);
-    *list = NULL;
+  if (!tmp->next) {
+    result = tmp->data;
+    free(tmp);
+    tmp = NULL;
     return result;
   }
   else {
     node* prev_node = NULL;
-    while ((*list)->next != NULL) {
-      prev_node = *list;
-      *list = (*list)->next; 
+    while (tmp->next != NULL) {
+      prev_node = tmp;
+      tmp = tmp->next; 
     } 
   
     if (prev_node)
       prev_node->next = NULL;
 
-    result = (*list)->data; 
-    free(*list);
+    result = tmp->data; 
+    free(tmp);
     return result;
   }
 }
 
-void list_insert(node** list, int new_element, int position){
-  node* new_node = (node*)malloc(sizeof(node));
-  CHECK_ALLOC(new_node);
+    void list_insert(node** list, int new_element, int position){
+    node* new_node = (node*)malloc(sizeof(node));
+    CHECK_ALLOC(new_node);
   
-  new_node->data = new_element;
+    new_node->data = new_element;
 
-  if (*list == NULL) {
-    new_node->next = NULL;
-    *list = new_node; 
-  }
+    if (*list == NULL) {
+      new_node->next = NULL;
+      *list = new_node; 
+    }
   
-  else if (position == 0) {
-    new_node->next = *list;
-    *list = new_node; 
-  }
+    else if (position == 0) {
+      new_node->next = *list;
+      *list = new_node; 
+    }
   
-  else {
-    node* prev = NULL;
-    node* current = *list;
-    int counter = 0;
+    else {
+      node* prev = NULL;
+      node* current = *list;
+      int counter = 0;
 
-    while (current->next) {
-      prev = current;
+      while (current->next) {
+	prev = current;
 	current = current->next; 
 	++counter;
       
@@ -119,9 +122,11 @@ void list_insert(node** list, int new_element, int position){
 
   int list_remove(node** list, int position){
     int removed_el;
-    if (!*list)
+    if (!*list){
+      printf("Can't remove from a null list!\n");
       return -1;
-
+    }
+    
     if (!(*list)->next) {
       removed_el = (*list)->data;
       free(*list);
@@ -131,31 +136,34 @@ void list_insert(node** list, int new_element, int position){
     
     else {
       if (position == 0){
-	node* temp = *list;
+	node* tmp = *list;
 	removed_el = (*list)->data; 
 	*list = (*list)->next; 
-	free(temp);
+	free(tmp);
       }
       else {
 	int counter = 0;
-	node* temp = *list;
-	while (temp->next) {
-	  temp = temp->next;
-	  ++counter; 
-	  if (counter == (position-1)) 
+	node* current = *list;
+	node* prev = NULL;
+	while (current->next){
+	  prev = current;
+	  current = current->next;
+	  ++counter;
+	  
+	  if (counter == position){
+	    prev->next = current->next;
+	    node* remove = current; 
+	    current->next = NULL;
+	    removed_el = current->data;
+	    free(remove);
 	    break; 
-	} 
-	if (temp->next->next == NULL){
-	  removed_el = temp->next->data;
-	  node* remove = temp->next;
-	  temp->next = NULL;
-	  free(remove);
-	}
-	else {
-	  removed_el = temp->next->data;
-	  node* remove = temp->next;
-	  temp->next = temp->next->next;
-	  free(remove);
+	  }
+	  else if (current->next == NULL && position>counter){
+	    removed_el = current->data;
+	    node* remove = current;
+	    prev->next = NULL;
+	    free(remove);
+	  }
 	} 
       }
       return removed_el;
@@ -293,6 +301,14 @@ void test_list_reverse(void){
   list_free(&list);
 }
 
+void test_list_reverse_too_big_position(void){
+  node* list = list_create(0);
+  list_append(&list, 1);
+  list_remove(&list, 5);
+  ASSERT(list->next == NULL);
+  list_free(&list);
+}
+
 
 int main(void){
   test_list_create();
@@ -304,8 +320,9 @@ int main(void){
   test_list_insert_to_empty();
   test_list_insert_to_right_position();
   test_list_remove();
-  printf("reverse below:\n");
   test_list_reverse();
+  printf("reverse below:\n");
+  test_list_reverse_too_big_position();
   
   return 0;
 }
